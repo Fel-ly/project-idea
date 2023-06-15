e98db6de6c81b9f3642ad71c116645ba64ef2362
Fel-ly <kiokofpm@gmail.com>
2023-06-16 02:33:11 +0300

    site database

---
+++ connect.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Directions</title>
+</head>
+<body>
+    <h1>Directions</h1>
+
+    <?php
+    // Establish a database connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "askme";
+
+    $conn = new mysqli($servername, $root, $askme);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $destination = $_GET['destination'];
+    $transport = $_GET['transport'];
+
+    // Prepare and execute the database query
+    $stmt = $conn->prepare("SELECT stage, transport FROM destinations WHERE destination = ?");
+    $stmt->bind_param("s", $destination);
+    $stmt->execute();
+    $stmt->bind_result($stage, $dbTransport);
+    $stmt->fetch();
+    $stmt->close();
+
+    if ($stage && $dbTransport) {
+        if ($transport == $dbTransport) {
+            $message = "To go to $destination, take the $transport at $stage.";
+        } else {
+            $message = "The $transport is not available for $destination.";
+        }
+    } else {
+        $message = "Destination not found.";
+    }
+
+    echo $message;
+
+    // Close the database connection
+    $conn->close();
+    ?>
+
+</body>
+</html>
